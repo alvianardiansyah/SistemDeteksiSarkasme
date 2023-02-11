744dd7841fb0c3070a895757954e47ec1b829f8f
yazid29 <48250130+yazid29@users.noreply.github.com>
2023-02-11 09:59:40 +0700

    Delete download stopwords

--- Stream_DeteksiSarkasme.py
+++ Stream_DeteksiSarkasme.py
@@ -4,7 +4,6 @@ import preProcessingModul
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
 import nltk
-nltk.download('stopwords')
 sw=nltk.corpus.stopwords.words("indonesian")
 
 # title and description
